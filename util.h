2ca74e0c2f66cea087e1b31ad19c6270d5054d75
ankang <ankanggao@ustc.edu.cn>
2022-09-08 14:15:01 +0800

    fix undefined size_t

--- util.h
+++ util.h
@@ -1,7 +1,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 #include <vector>
-
+#include <cstddef>
 double distance(std::vector<double> p0, std::vector<double> p1);
 double distance(std::vector<std::vector<double> > p);
 bool NanString(const char *buffer, size_t len);
